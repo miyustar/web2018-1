592ca74355cf8567747a162a8f3d2ec7ae1e23ec
zm17438 <zm17438@josai.ac.jp>
2018-07-29 22:02:55 -0400

    message

---
+++ ex6-1.js
@@ -0,0 +1,6 @@
+function sayhello61(){ alert('Hello,Function');}
+
+ var who='world';
+function sayhello61(){alert('Hello,'+who+'!');}
+function taro(){who='太郎が来た';}
+function hanako(){who='花子が来た';}
---
+++ myjs.js
@@ -0,0 +1,4 @@
+alert('Hello,Miyu!');
+//alert('Hello,Miyu!');
+alert(2+3);
+alert(Math.sprt(1+1));
